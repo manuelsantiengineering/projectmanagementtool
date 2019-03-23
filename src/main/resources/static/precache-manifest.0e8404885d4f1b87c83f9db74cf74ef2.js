4ea1dbcb460e912d7b69eb55f045b6ef5991d2d7
Manny <manuelsantiengineering@gmail.com>
2019-03-23 01:31:07 -0400

    Added the react built code.

---
+++ src/main/resources/static/precache-manifest.0e8404885d4f1b87c83f9db74cf74ef2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "88bc3f2233502ce55e7e",
+    "url": "/static/js/main.01cdc97c.chunk.js"
+  },
+  {
+    "revision": "464ced67e2463469d545",
+    "url": "/static/js/2.aefb620e.chunk.js"
+  },
+  {
+    "revision": "88bc3f2233502ce55e7e",
+    "url": "/static/css/main.d6ce7e6f.chunk.css"
+  },
+  {
+    "revision": "464ced67e2463469d545",
+    "url": "/static/css/2.1a13ac11.chunk.css"
+  },
+  {
+    "revision": "968bc2a1087cd1b3f8ab129f6c844ba8",
+    "url": "/index.html"
+  }
+];
