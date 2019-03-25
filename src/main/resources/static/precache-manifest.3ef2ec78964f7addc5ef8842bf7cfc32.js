6367fa65a7051695a298566d90e59d5a5a67e72b
Manuel Santiago <manuelsantiengineering@gmail.com>
2019-03-25 15:53:39 -0400

    Fixed React components using href

---
+++ src/main/resources/static/precache-manifest.3ef2ec78964f7addc5ef8842bf7cfc32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bb257281bb27ad178c70",
+    "url": "/static/js/main.e386e040.chunk.js"
+  },
+  {
+    "revision": "089fbf0476399d6e2409",
+    "url": "/static/js/2.aefb620e.chunk.js"
+  },
+  {
+    "revision": "bb257281bb27ad178c70",
+    "url": "/static/css/main.90cf57b3.chunk.css"
+  },
+  {
+    "revision": "089fbf0476399d6e2409",
+    "url": "/static/css/2.4c9acbba.chunk.css"
+  },
+  {
+    "revision": "79f09e4905b05890e1a1877879cc0f96",
+    "url": "/index.html"
+  }
+];
